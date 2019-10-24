5017a8a6d62698386a645df2ace8cf1f314a02b8
Maria Goenko <masha@dev.by>
2019-10-25 00:10:43 +0300

    feat: add slider + some code refactoring

--- script/index.js
+++ script/index.js
@@ -1,36 +1,48 @@
 (function() {
   let panelTitle = document.querySelector('.education--panel_title');
+  panelTitle.addEventListener('click', toggleExpansionPanel);
 
-  panelTitle.addEventListener('click', function() {
+  let leftArrow = document.querySelector('.slider--arrow-left');
+  leftArrow.addEventListener('click', showPrevious);
+
+  let rightArrow = document.querySelector('.slider--arrow-right');
+  rightArrow.addEventListener('click', showNext);
+
+  function showPrevious() {
+    showNext();
+  }
+
+  function showNext() {
+    let curItem = document.querySelector('.slider_item-active');
+    let nextItem = document.querySelector('.slider_item-active + div');
+
+    if(!nextItem) {
+      nextItem = document.querySelector('.slider_item:first-child');
+    }
+
+    curItem.classList.remove('slider_item-active');
+    nextItem.classList.add('slider_item-active');
+  }
+
+  function toggleExpansionPanel() {
     let listContainer = document.querySelector('.education--list_container');
     let isShow = listContainer.classList.contains('education--list_container-hidden');
+    let originalHeight = isShow ? 0 : parseInt(listContainer.style.height);
+    let start = Date.now();
 
-    if(isShow) {
-      let start = Date.now();
-      let timer = setInterval(function() {
-        let timePassed = Date.now() - start;
-    
-        if (timePassed >= 1100) {
-          clearInterval(timer);
-          return;
-        }
-        listContainer.style.height = timePassed / 5 + 'px';
-      }, 20);
-    }
-    else {
-      let originalHeight = listContainer.style.height;
-      let start = Date.now();
-      let timer = setInterval(function() {
-        let timePassed = Date.now() - start;
-    
-        if (timePassed >= 1100) {
-          clearInterval(timer);
-          return;
-        }
-        listContainer.style.height = (parseInt(originalHeight) - timePassed / 5) + 'px';
-      }, 20);
-    }
-    
     listContainer.classList.toggle('education--list_container-hidden');
-  });
+
+    let timer = setInterval(function() {
+      let timePassed = Date.now() - start;
+  
+      if (timePassed >= 1100) {
+        clearInterval(timer);
+        return;
+      }
+
+      let curHeight = isShow ? (timePassed / 5) : (originalHeight - timePassed / 5);
+      listContainer.style.height = curHeight + 'px';
+
+    }, 20);
+  }
 })();
